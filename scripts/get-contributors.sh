b649eb35ecc7cd5520764fc29aea9f3235e48b77
Chris Penner <christopher.penner@gmail.com>
2020-02-11 16:27:46 -0600

    Fix broken numContributors

--- scripts/get-contributors.sh
+++ scripts/get-contributors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 contributors=$(git shortlog --summary --numbered --email | cut -f2)
-numContributors=$(echo contributors | wc -l)
+numContributors=$(echo "$contributors" | wc -l)
 
 jq -n --argjson "numContributors" "$numContributors" --arg "contributors" "$contributors" '{numContributors: $numContributors, contributors: $contributors | split("\n")}' >&3
