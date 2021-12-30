659d72ce2d7e4865f4724cfc61e29bda2b4e10c8
Ben Stolz <bstolz@vendasta.com>
2021-12-30 15:18:57 -0600

    add script to change gosdk logging version in any repo

---
+++ scripts/change_gosdk_logging_version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SDK_VERSION=$(cat go.mod | grep "github.com/vendasta/gosdks/logging" | awk '{print $2}')
+
+echo "Logging SDK version on master: ${SDK_VERSION}"
+sed -i '' 's|.*github.com/vendasta/gosdks/logging.*|\tgithub.com/vendasta/gosdks/logging v1.12.0|' go.mod
+echo "Logging SDK version for PR   : ${SDK_VERSION}"
