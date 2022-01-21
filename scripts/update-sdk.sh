93cfdd60ce5b6b117f1d6d5bfecff30015ffdbf1
Ben Stolz <bstolz@vendasta.com>
2022-01-21 09:37:01 -0600

    feat(updatesdk): cleanup troubleshooting, usability improvements

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -2,7 +2,9 @@
 
 file_name=./go.mod
 sdk_name=github.com/vendasta/event-broker/sdks/go
-gh --version
+sdk_version=@1.4.0
+sdk="$sdk_name$sdk_version"
+
 sdk_file_version() {
     grep $sdk_name $file_name | awk '{print $2}'
 }
@@ -16,10 +18,12 @@ else
   then
     actual=$(sdk_file_version)
     echo "$sdk_name version on master is: $actual"
-    go get github.com/vendasta/event-broker/sdks/go@v1.4.0
+
+    go get $sdk
     go mod vendor
+
     actual=$(sdk_file_version)
-    echo "$sdk_name version on PR is: $actual"
+    echo "$sdk_name version for PR is: $actual"
   else
       echo "Repo doesn't use $sdk_name"
       exit 10
