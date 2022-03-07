51969fcaeca3662808a491a2ddf03b3728c7f4b9
Ben Stolz <bstolz@vendasta.com>
2022-03-07 09:15:01 -0600

    Update scripts/change_gosdk_logging_version.sh
    Co-authored-by: Conrad Berenik <cberenik@vendasta.com>

--- scripts/change_gosdk_logging_version.sh
+++ scripts/change_gosdk_logging_version.sh
@@ -2,7 +2,7 @@
 SDK_VERSION=$(cat go.mod | grep "github.com/vendasta/gosdks/logging" | awk '{print $2}')
 
 echo "Logging SDK version on master: ${SDK_VERSION}"
-sed -i '' 's|.*github.com/vendasta/gosdks/logging.*|\tgithub.com/vendasta/gosdks/logging v1.12.0|' go.mod
+sed -i '' 's|.*github.com/vendasta/gosdks/logging.*|\tgithub.com/vendasta/gosdks/logging v1.13.0|' go.mod
 echo "Logging SDK version for PR   : ${SDK_VERSION}"
 
 go mod vendor
