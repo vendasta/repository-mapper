22549481e4658d0c53898815a2a85f6e53c33a3d
Conrad <cberenik@vendasta.com>
2022-04-14 11:29:18 -0600

    Look for a min logging version and get latest if below

--- scripts/update-logging.sh
+++ scripts/update-logging.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-WANTED='v1.14.0'
+MIN='v1.13.3'
 SDK_VERSION=$(cat go.mod | grep "github.com/vendasta/gosdks/logging" | awk '{print $2}')
 
 echo "Logging SDK version on master: ${SDK_VERSION}"
-if [[ SDK_VERSION < WANTED ]]
+if [[ SDK_VERSION < MIN ]]
   then
-    sed -i '' 's|.*github.com/vendasta/gosdks/logging.*|\tgithub.com/vendasta/gosdks/logging v1.14.0|' go.mod
-    echo "Logging SDK version for PR   : ${SDK_VERSION}"
-
+    go get github.com/vendasta/gosdks/logging
     go mod vendor
   else
     exit 10
