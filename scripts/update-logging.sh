4303b2b832a8fe0b54f15667b23a367ebba7ca71
Conrad <cberenik@vendasta.com>
2022-04-14 13:26:08 -0600

    Check for go.mod file

--- scripts/update-logging.sh
+++ scripts/update-logging.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 MIN='v1.13.3'
+
+if [ ! -f go.mod ]; then
+    echo "No go.mod file present"
+    exit 10
+fi
 SDK_VERSION=$(cat go.mod | grep "github.com/vendasta/gosdks/logging" | awk '{print $2}')
 
 echo "Logging SDK version on master: ${SDK_VERSION}"
