17b12ff71a45107eb2c6a89261704b879be622e5
Conrad <cberenik@vendasta.com>
2022-04-19 13:38:35 -0600

    Make int comparisons for version number

--- scripts/update-logging.sh
+++ scripts/update-logging.sh
@@ -2,17 +2,31 @@
 MIN='v1.13.3'
 
 if [ ! -f go.mod ]; then
-    echo "No go.mod file present, skipping"
-    exit 10
+  echo "No go.mod file present, skipping"
+  exit 10
 fi
 SDK_VERSION=$(cat go.mod | grep "github.com/vendasta/gosdks/logging" | awk '{print $2}')
 
+if [[ -z $SDK_VERSION ]]; then
+  echo "logging sdk not present, skipping"
+  exit 10
+fi
+
+SDK_PIECES=(${SDK_VERSION//./ })
+
 echo "Logging SDK version on master: ${SDK_VERSION}"
-if [[ SDK_VERSION < MIN ]]
-  then
+if [[ ${SDK_PIECES[1]} -lt 13 ]]; then
+  go get github.com/vendasta/gosdks/logging
+  go mod vendor
+elif [[ ${SDK_PIECES[1]} -eq 13 ]]; then
+  if [[ ${SDK_PIECES[2]} -lt 3 ]]; then
     go get github.com/vendasta/gosdks/logging
     go mod vendor
   else
     echo "Logging version high enough, skipping"
     exit 10
+  fi
+else
+  echo "Logging version high enough, skipping"
+  exit 10
 fi
